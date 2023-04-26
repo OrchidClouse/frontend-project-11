e3f3d6f85e3757695ae65f6bdf4324bf11a0c550
OrchidClouse <maks67topcheg@yandex.ru>
2023-04-26 14:35:20 +0300

    WORK VALIDATE!!!!!!

--- src/js/validation.js
+++ src/js/validation.js
@@ -1,18 +1,31 @@
 import * as Yup from 'yup'
 import axios from 'axios'
 
-const schema = Yup.object().shape({
-    url:Yup.string().test('valid','invalid', async (value) =>{
-      const response = await axios.get(`https://allorigins.hexlet.app/get?url=${value.trim()}`)
-      return response.data.contents !== null
-    })
-  })
-  export async function valid(data){
+// const schema = Yup.object().shape({
+//     url:Yup.string().test('valid','invalid', async (value) =>{
+//       const response = await axios.get(`https://allorigins.hexlet.app/get?url=${value.trim()}`)
+//       return response.data.contents !== null
+//     })
+//   })
+//   export async function valid(data){
+//     try{
+//       await schema.validate({url: data})
+//       return {success: true,errors:[]}
+//     }catch(er){
+//         console.log()
+//       return {success: false,errors: er.errors}
+//     }
+//   }
+
+const re = /^((https?|ftp):\/\/)?(www.)?(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i
+
+const scheme = Yup.string().matches(re,'URL is not valid')
+
+export async function valid(url){
     try{
-      await schema.validate({url: data})
-      return {success: true,errors:[]}
-    }catch(er){
-        console.log()
-      return {success: false,errors: er.errors}
+        await scheme.validate(url)
+        return {success: true, errors: []}
+    }catch(err){
+        return {success: false, errors: err.errors}
     }
-  }
+}
