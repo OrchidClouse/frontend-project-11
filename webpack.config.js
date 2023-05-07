6985602448bfbdc2642123952fb510ec524554b7
OrchidClouse <maks67topcheg@yandex.ru>
2023-05-07 15:47:57 +0300

    fix dist directory

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const isProduction = process.env.NODE_ENV == 'production';
 
 
 const config = {
+    mode: "production",
     entry: './src/js/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
